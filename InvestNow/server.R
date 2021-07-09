8f2f264aa858e4528edaa036e5e0538af7d7c531
Victor Nugraha <victor.nugraha37@gmail.com>
2021-07-09 10:59:05 +0700

    Add files via upload

--- InvestNow/server.R
+++ InvestNow/server.R
@@ -1,10 +1,23 @@
 function(input, output) {
     
-  output$slickr <- renderSlickR({
+#NAV BAR TAB PANEL HOME PAGE---  
+  
+  # Output slide show slicker---
+  output$slide_show <- renderSlickR({
+    
     imgs <- list.files("C:/Users/user/Desktop/Algoritma/DCD/InvestNow/www/home", 
                        pattern=".jpg", 
                        full.names = TRUE)
     slickR(imgs)
   })
+  
+  # Output render image---
+  output$image <- renderImage({
+    
+    list(src = "C:/Users/user/Desktop/Algoritma/DCD/InvestNow/www/2.png",
+         width = "100%",
+         height = "100%")
+    
+  }, deleteFile = F)
   
 }
--- InvestNow/ui.R
+++ InvestNow/ui.R
@@ -1,43 +1,52 @@
 #NAV BAR---
 
-navbarPage(title = div(img(src="stock-market-white.png", 
+navbarPage(title = div(img(src="stock-market.png", 
                            height = '40px', 
                            width = '40px'), 
                        "InvestNow"),
-           theme = bs_theme(bootswatch = "solar",
+           theme = bs_theme(bootswatch = "minty",
                             # bg = "#002B36",
                             # fg = "white",
                             # primary = "maroon",
                             base_font = font_google("Prompt"),
                             code_font = font_google("JetBrains Mono")
                             ),
-           
+
 #------------------------------
 #NAV BAR TAB PANEL HOME PAGE---           
            tabPanel(title = "Home", 
-                    #Slide Photo Output
-                    slickROutput("slickr", 
-                                 width ="1100px",
-                                 height = "300px")
+                    fluidPage(
+                      fluidRow(
+                        #Slide Photo Output---
+                          slickROutput("slide_show", 
+                                       width ="1100px",
+                                       height = "300px")
+                          ),
+                      
+                        #Render image below slide photo---
+                          imageOutput("image")
+                      )
                     ),
 
 #------------------------------
-#NAV BAR TAB PANEL HOME PAGE---  
+#NAV BAR TAB PANEL TRADING PLAN---  
            tabPanel(title = "Trading Plan",
                     fluidPage(
-                      box(
-                        width = 8
+                      fluidRow(
+                    tabBox(width = 8
+                    ),
+                    tabBox(width = 4)
                       )
                     )
                   ),
 
 #------------------------------
-#NAV BAR TAB PANEL HOME PAGE---  
+#NAV BAR TAB PANEL PROFIT---  
            tabPanel(title = "Profit",
                     "Comparison"),
 
 #------------------------------
-#NAV BAR TAB PANEL HOME PAGE---  
+#NAV BAR TAB PANEL PROFILE---  
            tabPanel(title = "Profile",
                     "Profile")
 )
